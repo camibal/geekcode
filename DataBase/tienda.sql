2cd63c6113e4f045a70930c1c9bf5e085506bcf7
Camilo Ballesteros <camibal1995@gmail.com>
2020-06-20 14:53:14 -0500

    second commit

--- DataBase/tienda.sql
+++ DataBase/tienda.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.2
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 20-01-2020 a las 13:43:04
+-- Tiempo de generación: 20-06-2020 a las 21:46:59
 -- Versión del servidor: 10.4.11-MariaDB
--- Versión de PHP: 7.4.1
+-- Versión de PHP: 7.4.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -60,10 +59,8 @@ CREATE TABLE `carro` (
 --
 
 INSERT INTO `carro` (`id`, `id_cliente`, `id_producto`, `cant`) VALUES
-(8, 1, 2, 3),
-(34, 7, 21, 1),
-(35, 7, 17, 1),
-(36, 7, 18, 1);
+(40, 15, 19, 5),
+(41, 15, 21, 10);
 
 -- --------------------------------------------------------
 
@@ -97,23 +94,19 @@ CREATE TABLE `clientes` (
   `id` int(11) NOT NULL,
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `name` varchar(255) NOT NULL
+  `name` varchar(255) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `celular` varchar(15) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `clientes`
 --
 
-INSERT INTO `clientes` (`id`, `username`, `password`, `name`) VALUES
-(1, 'boscan', '123456', 'Anyelber'),
-(3, 'anyelber', 'anyelber', 'Anyelber'),
-(6, 'test', 'test', 'test'),
-(7, 'cambal', '123', 'camilo'),
-(8, 'camilo ballesteros', '123', 'camilo2'),
-(9, 'prueba', '123', 'camilo ballesteros'),
-(10, 'prueba2', '123', 'prueba'),
-(11, 'prueba3', '123', 'prueba2'),
-(13, 'admin', '123', 'admin');
+INSERT INTO `clientes` (`id`, `username`, `password`, `name`, `email`, `celular`) VALUES
+(13, 'admin', '123', 'admin', 'camilo@correo.com', '310235641'),
+(14, 'cambal', '12345', 'camilo ballesteros', 'camilo@correo.com', '3102354685'),
+(15, 'camilo andres ballesteros villalba', '123', 'camilo', 'camibal@gmail.com', '310236521');
 
 -- --------------------------------------------------------
 
@@ -134,12 +127,8 @@ CREATE TABLE `compra` (
 --
 
 INSERT INTO `compra` (`id`, `id_cliente`, `fecha`, `monto`, `estado`) VALUES
-(16, 7, '2020-01-16 00:01:55', 270000, 2),
-(17, 7, '2020-01-16 00:02:01', 0, 0),
-(18, 7, '2020-01-16 00:02:37', 283200, 0),
-(21, 7, '2020-01-16 00:04:04', 13200, 0),
-(22, 7, '2020-01-16 00:08:19', 120000, 1),
-(23, 7, '2020-01-16 00:44:26', 87400, 1);
+(25, 13, '2020-06-18 12:56:15', 10000, 0),
+(26, 14, '2020-06-18 13:07:59', 45000, 0);
 
 -- --------------------------------------------------------
 
@@ -156,16 +145,6 @@ CREATE TABLE `pagos` (
   `fecha` datetime NOT NULL,
   `estado` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Volcado de datos para la tabla `pagos`
---
-
-INSERT INTO `pagos` (`id`, `id_cliente`, `id_compra`, `comprobante`, `nombre`, `fecha`, `estado`) VALUES
-(4, 1, 8, '01055363.png', 'Anyelber Boscan', '2019-05-22 18:05:53', 1),
-(5, 7, 13, '232624321.png', 'pago roductos', '2020-01-15 17:26:24', 1),
-(6, 7, 14, '2330207.png', 'compra nueva', '2020-01-15 17:30:20', 1),
-(7, 7, 15, '0103296.png', 'ya', '2020-01-15 19:03:29', 1);
 
 -- --------------------------------------------------------
 
@@ -220,28 +199,7 @@ CREATE TABLE `productos_compra` (
 --
 
 INSERT INTO `productos_compra` (`id`, `id_compra`, `id_producto`, `cantidad`, `monto`) VALUES
-(1, 3, 5, 7, 400),
-(2, 3, 4, 4, 1000),
-(3, 3, 3, 4, 1200),
-(4, 3, 2, 4, 200),
-(5, 3, 1, 4, 1000),
-(6, 4, 5, 1, 400),
-(7, 4, 7, 1, 1000),
-(8, 5, 5, 1, 400),
-(9, 5, 4, 1, 1000),
-(10, 5, 7, 1, 1000),
-(11, 6, 5, 1, 400),
-(12, 7, 7, 1, 1000),
-(13, 7, 2, 1, 200),
-(14, 7, 1, 1, 1000),
-(33, 16, 19, 10, 45000),
-(34, 18, 20, 1, 13200),
-(35, 18, 19, 10, 45000),
-(36, 21, 20, 1, 13200),
-(37, 22, 21, 12, 10000),
-(38, 23, 19, 1, 45000),
-(39, 23, 21, 1, 10000),
-(40, 23, 15, 5, 11200);
+(42, 26, 13, 1, 45000);
 
 --
 -- Índices para tablas volcadas
@@ -309,7 +267,7 @@ ALTER TABLE `admins`
 -- AUTO_INCREMENT de la tabla `carro`
 --
 ALTER TABLE `carro`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
 
 --
 -- AUTO_INCREMENT de la tabla `categorias`
@@ -321,13 +279,13 @@ ALTER TABLE `categorias`
 -- AUTO_INCREMENT de la tabla `clientes`
 --
 ALTER TABLE `clientes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT de la tabla `compra`
 --
 ALTER TABLE `compra`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 
 --
 -- AUTO_INCREMENT de la tabla `pagos`
@@ -345,7 +303,7 @@ ALTER TABLE `productos`
 -- AUTO_INCREMENT de la tabla `productos_compra`
 --
 ALTER TABLE `productos_compra`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
